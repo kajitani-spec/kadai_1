698a63932b7a476eb706c87b772b5416fbd78429
kajitani <to.kajitani@shimade.ac.jp>
2025-11-04 14:59:34 +0900

    終わり

--- kadai3.cpp
+++ kadai3.cpp
@@ -3,26 +3,26 @@
 
 void convert(int year) {
     if(year>=2019) {
-      printf("R%d\n",year-2018);
+      printf("令和%d年\n",year-2018);
     }
     else if(year>=1989) {
-        printf("H%d\n",year-1988);
+        printf("平成%d年\n",year-1988);
     }
     else if(year>=1926) {
-        printf("S%d\n",year-1925);
+        printf("昭和%d年\n",year-1925);
     }
     // TODO: 実装1887
 }
 
 void convert(const char era[], int year) {
     if (era[0] == 'R') {
-        printf("R%d\n",year);
+        printf("%d\n",2018+year);
     }
     else if(era[0] == 'H') {
-        printf("H%d\n",year);
+        printf("%d\n",1988+year);
     }
     else if(era[0] == 'S') {
-        printf("S%d\n",year);
+        printf("%d\n",1925+year);
     }
 
     // TODO: 実装R7
